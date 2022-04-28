ae0b11b22d9a941f324946eaabcfa9438aac2ed2
timkoopmans <tim.koops@gmail.com>
2022-04-28 08:33:23 +0800

    start logging

--- programs/solana-playground/src/lib.rs
+++ programs/solana-playground/src/lib.rs
@@ -4,9 +4,14 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod solana_playground {
+    use anchor_lang::solana_program::log::sol_log_compute_units;
+    use anchor_lang::solana_program::sysvar;
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let now = sysvar::clock::Clock::get().unwrap().unix_timestamp as u64;
+        msg!("Now = {:?}", now);
+        sol_log_compute_units();
         Ok(())
     }
 }
